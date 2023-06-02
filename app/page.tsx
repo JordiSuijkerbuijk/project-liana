a0a185bb603b76523591fae1e03bf31d6278c492
Jordi Suijkerbuijk <j.suijkerbuijk@hotmail.com>
2023-06-03 01:07:36 +0200

    Litte section check

--- app/page.tsx
+++ app/page.tsx
@@ -1,80 +1,11 @@
 'use client';
 
-import Image from 'next/image';
-import { useEffect, useRef } from 'react';
-import image from '../public/images/hover.jpg';
+import ProjectsSection from './sections/ProjectsSection';
 
 export default function Home() {
-  const projectsContainerRef = useRef<HTMLInputElement>(null);
-
-  function handleMouseMoveListener(event: MouseEvent) {
-    if (projectsContainerRef?.current === null) return;
-    const images = projectsContainerRef.current.querySelectorAll<HTMLElement>('img');
-    console.log('event', event);
-    for (let i = 0; i < images.length; i++) {
-      const image = images[i];
-      if (image && image !== null) {
-        // image.style.transform = `translateX(${event.x}px)
-        // translateY(${event.y}px)`;
-        image.style.top = `${event.y}px`;
-        image.style.left = `${event.x}px`;
-      }
-    }
-  }
-
-  function intersectionHandler(entries: IntersectionObserverEntry[]) {
-    entries.forEach(function (entry: IntersectionObserverEntry): void {
-      if (entry.isIntersecting) {
-        window.addEventListener('mousemove', handleMouseMoveListener);
-      } else {
-        window.removeEventListener('mousemove', handleMouseMoveListener);
-      }
-    });
-  }
-
-  useEffect(() => {
-    const observer = new IntersectionObserver(intersectionHandler);
-    const ref = projectsContainerRef.current;
-    if (ref !== null) {
-      observer.observe(ref);
-    }
-
-    return () => {
-      if (ref !== null) {
-        observer.unobserve(ref);
-        window.removeEventListener('mousemove', handleMouseMoveListener);
-      }
-    };
-  });
-
   return (
-    <main className='flex min-h-screen py-96 flex-col items-center justify-between p-24'>
-      {/* The projects section */}
-      <div className='grid grid-cols-4 lg:grid-cols-12 [contain:paint] gap-x-8'>
-        <div className='col-span-4 gap-y-4 flex flex-col sticky top-8 left-0 h-fit'>
-          <h2 className='text-2xl'>Animated text</h2>
-          <p className='max-w-lg text-sm'>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-            exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
-            dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-            mollit anim id est laborum
-          </p>
-        </div>
-        <div className='col-span-8 flex flex-col items-end gap-y-8' ref={projectsContainerRef}>
-          {/* project card */}
-          <div className='h-96 w-full max-w-4xl bg-black relative overflow-hidden'>
-            <Image
-              {...image}
-              alt='hover image'
-              className='h-36 w-36 absolute -top-full rounded-lg'
-            />
-          </div>
-          <div className='h-96 w-full max-w-4xl bg-black'></div>
-          <div className='h-96 w-full max-w-4xl bg-black'></div>
-        </div>
-      </div>
+    <main className='flex flex-col items-center justify-between min-h-screen p-24 py-96'>
+      <ProjectsSection />
     </main>
   );
 }
---
+++ app/sections/ProjectsSection.tsx
@@ -0,0 +1,117 @@
+'use client';
+
+import Image from 'next/image';
+import { MouseEvent as MouseEventReact, useEffect, useRef, useState } from 'react';
+import image from '../../public/images/hover.jpg';
+
+export default function ProjectsSection() {
+  const projects = [1, 2, 3];
+  const projectsContainerRef = useRef<HTMLInputElement>(null);
+  const sectionRef = useRef<HTMLInputElement>(null);
+  const [images, setImages] = useState<HTMLImageElement[]>([]);
+
+  let scaled = false;
+  let ticking = false;
+
+  function mouseMoveHandler(
+    event: MouseEvent,
+    currentImage: HTMLImageElement,
+    currentProject: HTMLElement
+  ) {
+    if (!currentImage) return;
+
+    if (!ticking) {
+      requestAnimationFrame(() => {
+        if (projectsContainerRef.current && sectionRef.current) {
+          currentImage.style.transform = `translateX(${
+            event.x -
+            projectsContainerRef.current.offsetLeft -
+            sectionRef.current.offsetLeft -
+            currentImage.clientWidth / 2
+          }px) translateY(${
+            event.y -
+            sectionRef.current.offsetTop -
+            currentProject.offsetTop +
+            window.scrollY -
+            currentImage.clientHeight / 2
+          }px)`;
+        }
+
+        ticking = false;
+      });
+
+      ticking = true;
+    }
+  }
+
+  function mouseEnterHandler(e: MouseEventReact, key: number) {
+    if (projectsContainerRef.current) {
+      const currentProject = Array.from(projectsContainerRef.current.children)[key] as HTMLElement;
+      const currentImage = images[key];
+
+      if (!scaled && currentImage) {
+        currentImage.style.opacity = '100';
+        scaled = true;
+      }
+
+      currentProject.addEventListener('mousemove', (event) =>
+        mouseMoveHandler(event, currentImage, currentProject)
+      );
+    }
+  }
+
+  function mouseLeaveHandler(e: MouseEventReact, key: number) {
+    if (projectsContainerRef.current) {
+      const currentProject = Array.from(projectsContainerRef.current.children)[key] as HTMLElement;
+      const currentImage = images[key];
+
+      if (scaled && currentImage) {
+        currentImage.style.opacity = '0';
+        scaled = false;
+      }
+
+      currentProject.removeEventListener('mousemove', (event) =>
+        mouseMoveHandler(event, currentImage, currentProject)
+      );
+    }
+  }
+
+  useEffect(() => {
+    if (projectsContainerRef.current) {
+      setImages(Array.from(projectsContainerRef.current.querySelectorAll('img')));
+    }
+  }, []);
+
+  return (
+    <div className='grid grid-cols-4 lg:grid-cols-12 gap-x-8 [contain:paint]' ref={sectionRef}>
+      <div className='sticky left-0 flex flex-col col-span-4 gap-y-4 top-8 h-fit'>
+        <h2 className='text-2xl'>Animated text</h2>
+        <p className='max-w-lg text-sm'>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+          ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+          ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
+          sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+          est laborum
+        </p>
+      </div>
+      <div className='flex flex-col items-end col-span-8 gap-y-8' ref={projectsContainerRef}>
+        {/* project card */}
+        {projects.map((item, key) => (
+          <div
+            className='relative w-full max-w-4xl overflow-hidden bg-black rounded-lg h-96'
+            onMouseOver={(e) => mouseEnterHandler(e, key)}
+            onMouseLeave={(e) => mouseLeaveHandler(e, key)}
+            key={key}
+          >
+            <Image
+              {...image}
+              alt='hover image'
+              className='z-10 transition-opacity duration-150 rounded-lg opacity-0 h-36 w-36 scale-25'
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
