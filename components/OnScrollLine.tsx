8f0806376aa99dbf16cdf9d2c3412a9955f43f7b
Tristan Kirwan <33831870+TristanKirwan@users.noreply.github.com>
2023-10-06 20:27:25 +0200

    Fixed scrollbar due to scrollLine

--- components/OnScrollLine.tsx
+++ components/OnScrollLine.tsx
@@ -32,29 +32,31 @@ export default function OnScrollLine() {
   );
 
   return (
-    <div
-      className='absolute w-[calc(100%_+_40px)] -left-4 top-16'
-      ref={sectionContainer as RefObject<HTMLDivElement>}
-    >
-      <svg viewBox='0 0 2000 2000' className='lineAnimation'>
-        <defs>
-          <linearGradient id='linear' x1='0%' y1='0%' x2='100%' y2='0%'>
-            <stop offset='0%' stop-color='#c295d8' />
-            <stop offset='100%' stop-color='#f1c6d3' />
-          </linearGradient>
-        </defs>
-        <path
-          d='M0,163.475S562.8,728.329,529.694,1235.953c-26.9,344.249-535.212,353.129-369.682,44.141,231.741-357.549,717.294-314.506,899.376-148.976s192.219,207.336,422.106,54.078,199.235,567.055,485.317,541.512'
-          ref={svgRef}
-          fill='none'
-          strokeLinecap='round'
-          stroke='url(#linear)'
-          strokeWidth='35'
-          pathLength='1'
-          strokeDashoffset='1'
-          style={{ strokeDashoffset: 1 }}
-        />
-      </svg>
+    <div className="absolute top-0 left-0 w-full overflow-hidden aspect-square">
+      <div
+        className='absolute w-[calc(100%_+_40px)] -left-4 top-16'
+        ref={sectionContainer as RefObject<HTMLDivElement>}
+      >
+        <svg viewBox='0 0 2000 2000' className='lineAnimation'>
+          <defs>
+            <linearGradient id='linear' x1='0%' y1='0%' x2='100%' y2='0%'>
+              <stop offset='0%' stop-color='#c295d8' />
+              <stop offset='100%' stop-color='#f1c6d3' />
+            </linearGradient>
+          </defs>
+          <path
+            d='M0,163.475S562.8,728.329,529.694,1235.953c-26.9,344.249-535.212,353.129-369.682,44.141,231.741-357.549,717.294-314.506,899.376-148.976s192.219,207.336,422.106,54.078,199.235,567.055,485.317,541.512'
+            ref={svgRef}
+            fill='none'
+            strokeLinecap='round'
+            stroke='url(#linear)'
+            strokeWidth='35'
+            pathLength='1'
+            strokeDashoffset='1'
+            style={{ strokeDashoffset: 1 }}
+          />
+        </svg>
+      </div>
     </div>
   );
 }
