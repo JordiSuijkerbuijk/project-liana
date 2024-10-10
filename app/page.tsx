6fed01e1e9ed83a137bc04a8922f9e184b23f348
Tristan <33831870+TristanKirwan@users.noreply.github.com>
2024-10-10 13:44:24 +0200

    Revert "Added base metadata"
    This reverts commit 5f6beb7e308e785e5daa2c1ddee7034d86e155e7.

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,17 @@
-import type { Metadata } from 'next';
+import { SliceZone } from '@prismicio/react';
 
-// import { getClient } from '@/helpers/getClient';
+import { getClient } from '@/helpers/getClient';
 
-export const metadata: Metadata = {
-  title: 'Liana - Creating digital experiences',
-  description:
-    'Liana is a young creative digital agency that makes powerful and engaging web-based solutions.',
-};
+import { components } from './prismic/slices';
 
 export default async function Home() {
-  // const client = getClient();
-  // const data = await client?.getSingle('home');
+  const client = getClient();
+  const data = await client?.getSingle('home');
 
   return (
     <main className="relative z-10 flex flex-col items-center justify-between min-h-screen">
       <div className="relative z-20 w-full">
-        {/* <SliceZone slices={data?.data?.slices} components={components} /> */}
+        <SliceZone slices={data?.data?.slices} components={components} />
       </div>
 
       <div className="fixed top-0 left-0 flex justify-around w-full h-0 lines-animation">
