5f6beb7e308e785e5daa2c1ddee7034d86e155e7
Tristan <33831870+TristanKirwan@users.noreply.github.com>
2024-10-10 13:44:15 +0200

    Added base metadata

---
+++ app/about-us/page.tsx
@@ -0,0 +1,11 @@
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Liana - About Us',
+  description:
+    'Liana is a young creative digital agency that makes powerful and engaging web-based solutions.',
+};
+
+export default function AboutUsPage() {
+  return <main>About us!</main>;
+}
